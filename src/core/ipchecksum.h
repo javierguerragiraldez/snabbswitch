40ac354356617f3ab7a8266ca504bc8345b9769a
Javier Guerra <javier@guerrag.com>
2015-03-10 23:59:24 -0500

    first draft of csum app

--- src/core/ipchecksum.h
+++ src/core/ipchecksum.h
@@ -2,7 +2,7 @@
 void update_checksum(unsigned char *buffer, int len, uint32_t initial, int cksum_pos);
 
 // Calculate and return a ones-complement ip checksum.
-uint16_t calc_checksum(unsigned char *buffer, int len, uint32_t initial, int cksum_pos)
+uint16_t calc_checksum(unsigned char *buffer, int len, uint32_t initial, int cksum_pos);
 
 // Incrementally update checksum when modifying a 16-bit value.
 void checksum_update_incremental_16(uint16_t* checksum_cell,
